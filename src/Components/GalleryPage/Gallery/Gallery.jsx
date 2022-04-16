9aa3c402ce65fe1d4602c72625536d2dc926d9cf
hipster24930@gmail.com <Agasik19>
2022-04-16 16:19:44 +0300

    remove GalleryPage, add ContactPage

---
+++ src/Components/GalleryPage/Gallery/Gallery.jsx
@@ -0,0 +1,15 @@
+import classes from './Gallery.module.scss';
+
+
+const About = () => {
+    return (
+        <div className={classes.gallery_main}>
+            <div className={classes.gallery}>
+                <div className={classes.gallery_text}>GALLERY</div>
+            </div>
+        </div>
+
+    )
+}
+
+export default About;
